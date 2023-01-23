aa49cf47e9b38401c4af4cd74a5bea21f763d017
Seton Brown <setonb@gmail.com>
2023-01-23 13:17:52 -0500

    Updating styles for dark/light and modal

--- resources/js/nova/FormField.vue
+++ resources/js/nova/FormField.vue
@@ -7,7 +7,7 @@
   >
     <template #field>
       <div
-        class="flex flex-wrap mb-2 bg-white rounded-lg"
+        class="flex flex-wrap mb-2 bg-white rounded-lg dark:bg-gray-800"
         :class="`nova-eloquent-imagery-${resourceName}`"
       >
         <template v-if="field.isCollection">
@@ -66,7 +66,7 @@
             @click.prevent="$refs['uploadNewImageFromFileInput'].click()"
           >
             <!-- eslint-disable vue/max-attributes-per-line -->
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="72" width="72">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="72" width="72" fill="currentColor">
               <path fill="#888" d="M6 2h9a1 1 0 0 1 .7.3l4 4a1 1 0 0 1 .3.7v13a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2zm9 2.41V7h2.59L15 4.41zM18 9h-3a2 2 0 0 1-2-2V4H6v16h12V9zm-5 4h2a1 1 0 0 1 0 2h-2v2a1 1 0 0 1-2 0v-2H9a1 1 0 0 1 0-2h2v-2a1 1 0 0 1 2 0v2z"/>
             </svg>
             <!-- eslint-enable -->
--- resources/js/nova/ImageCard.vue
+++ resources/js/nova/ImageCard.vue
@@ -14,7 +14,9 @@
       :show="isPreviewImageModalOpen"
       modal-style="window"
       @close-via-escape="handleClose"
+      class="bg-gray-100 dark:bg-gray-700 px-6"
     >
+      <ModalHeader></ModalHeader>
       <img
         alt="An eloquent imagery image"
         class="block mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
@@ -54,6 +56,7 @@
           viewBox="0 0 24 24"
           width="24"
           height="24"
+          fill="currentColor"
         >
           <path d="M13 5.41V17a1 1 0 0 1-2 0V5.41l-3.3 3.3a1 1 0 0 1-1.4-1.42l5-5a1 1 0 0 1 1.4 0l5 5a1 1 0 1 1-1.4 1.42L13 5.4zM3 17a1 1 0 0 1 2 0v3h14v-3a1 1 0 0 1 2 0v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-3z" />
         </svg>
@@ -67,6 +70,7 @@
           viewBox="0 0 24 24"
           width="24"
           height="24"
+          fill="currentColor"
         >
           <path d="M2.59 13.41A1.98 1.98 0 0 1 2 12V7a5 5 0 0 1 5-5h4.99c.53 0 1.04.2 1.42.59l8 8a2 2 0 0 1 0 2.82l-8 8a2 2 0 0 1-2.82 0l-8-8zM20 12l-8-8H7a3 3 0 0 0-3 3v5l8 8 8-8zM7 8a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
         </svg>
@@ -81,6 +85,7 @@
           viewBox="0 0 24 24"
           width="24"
           height="24"
+          fill="currentColor"
         >
           <path d="M16.24 14.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1 0 0 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12 10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12l2.83 2.83z" />
         </svg>
@@ -102,7 +107,7 @@
     >
       <div class="w-screen">
         <div
-          class="w-2/3 m-auto bg-white select-text"
+          class="w-2/3 m-auto bg-white select-text dark:bg-gray-800"
           style="min-height: 12em"
         >
           <div class="w-full p-8 m-2">
@@ -166,6 +171,7 @@
                     viewBox="0 0 24 24"
                     width="24"
                     height="24"
+                    fill="currentColor"
                   >
                     <path d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v14h14V5H5zm11 7a1 1 0 0 1-1 1H9a1 1 0 0 1 0-2h6a1 1 0 0 1 1 1z" />
                   </svg>
@@ -184,6 +190,7 @@
               viewBox="0 0 24 24"
               width="24"
               height="24"
+              fill="currentColor"
             >
               <path d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v14h14V5H5zm8 6h2a1 1 0 0 1 0 2h-2v2a1 1 0 0 1-2 0v-2H9a1 1 0 0 1 0-2h2V9a1 1 0 0 1 2 0v2z"/>
             </svg>
